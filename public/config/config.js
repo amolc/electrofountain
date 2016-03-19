03e7de50b3460afdf85caa3a537409c3aae1f407
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-03-19 15:17:02 +0530

    config changed

--- public/config/config.js
+++ public/config/config.js
@@ -1,8 +1,8 @@
 if (document.location.hostname == "node.fountaintechies.com" ){
-	var baseUrl = "http://node.fountaintechies.com:9999/api/";
+	var baseUrl = "http://node.fountaintechies.com:8500/api/";
 
 } else {
 
-	var baseUrl = "http://localhost:9999/api/";
+	var baseUrl = "http://localhost:8500/api/";
 	
 }
